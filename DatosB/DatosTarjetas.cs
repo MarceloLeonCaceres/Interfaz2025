4a70d892036d63057658f1eb01d3a91d957389c1
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2025-03-27 11:02:22 -0500

    add DatosTarjetas con Dapper en DatosB proj

---
+++ DatosB/DatosTarjetas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+using ConexionDatos;
+using System.Threading.Tasks;
+
+namespace DatosB
+{
+    public static class DatosTarjetas
+    {
+        public static async Task<List<string>> GetTarjetasValidas()
+        {
+            string sql = "Select Serie FROM Cards;";
+            var accesoDatos = new SqlDataAccess();
+
+            var listado = await accesoDatos.ReadDataAsync<string, dynamic>(sql, null);
+            return listado;
+        }
+    }
+}
--- datosb/Properties/AssemblyInfo.cs
+++ datosb/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DatosB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("BioDevices Cia. Ltda.")]
 [assembly: AssemblyProduct("DatosB")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.25.3.27")]
+[assembly: AssemblyFileVersion("2.25.3.27")]
