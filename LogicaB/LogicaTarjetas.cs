e532410990ea6897fc7bd0ef29b59dae68121561
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2025-03-27 11:03:51 -0500

    add LogicaTarjeta

---
+++ LogicaB/LogicaTarjetas.cs
@@ -0,0 +1,25 @@
+﻿using DatosB;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LogicaB
+{
+    public static class LogicaTarjetas
+    {
+
+        public static async Task<List<string>> GetTarjetasValidas()
+        {
+            var listaEncriptada = await DatosTarjetas.GetTarjetasValidas();
+
+            if (listaEncriptada == null || listaEncriptada.Count == 0)
+            {
+                return new List<string>();
+            }
+
+            var AuxCripto = new Utilitarios.ClsEncriptacion();
+            var listaDesencriptada = AuxCripto.DesencriptarListaBIO(listaEncriptada);
+
+            return listaDesencriptada;
+        }
+    }
+}
