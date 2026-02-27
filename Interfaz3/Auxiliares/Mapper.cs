fb7c24cba9a68d64d5a03b884e8c2c43f11c18e7
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2026-02-27 10:57:35 -0500

    fix error ConvertToDesencriptedEmployees en Auxiliares\Mapper

--- Interfaz3/Auxiliares/Mapper.cs
+++ Interfaz3/Auxiliares/Mapper.cs
@@ -1,4 +1,5 @@
 ﻿using SDK;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
@@ -27,19 +28,44 @@ namespace AdminDispositivosBiometricos.Auxiliares
         public static List<SDKHelper.Employee> ConvertToDesencriptedEmployees(DataGridViewSelectedRowCollection selectedRows)
         {
             List<SDKHelper.Employee> empleados = new List<SDKHelper.Employee>();
-            Utilitarios.ClsEncriptacion encripter = new Utilitarios.ClsEncriptacion();
 
             foreach (DataGridViewRow row in selectedRows)
             {
-                SDKHelper.Employee empleado = new SDKHelper.Employee()
+                if (row.IsNewRow) continue;
+
+                Utilitarios.ClsEncriptacion encripter = new Utilitarios.ClsEncriptacion();
+
+                string idEmpleado = row.Cells["NumCA"]?.Value?.ToString() ?? "Desconocido";
+                string nombreEmpleado = row.Cells["NombreUsuario"]?.Value?.ToString() ?? "Sin Nombre";
+                object rawPass = row.Cells["mverifyPass"].Value;
+
+                var rawPrivilege = row.Cells["emPrivilege"].Value;
+                int privilege = 0;
+
+                if (rawPrivilege != null && rawPrivilege != DBNull.Value)
                 {
-                    pin = row.Cells["NumCA"].Value.ToString(),
-                    name = row.Cells["NombreUsuario"].Value.ToString(),
-                    password = encripter.Desencripta(row.Cells["mverifyPass"].Value.ToString()),
-                    privilege = (short)row.Cells["emPrivilege"].Value,
-                    cardNumber = row.Cells["cardNumber"].Value.ToString()
-                };
-                empleados.Add(empleado);
+                    privilege = Convert.ToInt32(rawPrivilege);
+                }
+
+                try
+                {
+                    var empleado = new SDKHelper.Employee()
+                    {
+                        pin = idEmpleado,
+                        name = nombreEmpleado,
+                        password = (rawPass == null || rawPass == DBNull.Value ||
+                string.IsNullOrWhiteSpace(rawPass.ToString()))
+                ? ""
+                : encripter.Desencripta(rawPass.ToString().Replace(" ", "+")),
+                        privilege = privilege,
+                        cardNumber = row.Cells["cardNumber"].Value?.ToString()?.Trim() ?? ""
+                    };
+                    empleados.Add(empleado);
+                }
+                catch (System.Exception ex)
+                {
+                    throw;
+                }
             }
             return empleados;
         }
