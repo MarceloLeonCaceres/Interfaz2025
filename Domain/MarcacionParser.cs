2c24adb773a6b96727b80f1d1f3cacc8d679a98f
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2026-03-12 11:44:41 -0500

    fix TryParse en MarcacionesParser en Domain

--- Domain/MarcacionParser.cs
+++ Domain/MarcacionParser.cs
@@ -18,20 +18,23 @@ namespace Domain
             if (parts.Length < 7)
                 return false;
 
-            if (parts[0] == "0")
+            if (parts[0] == "0" || parts[0].Trim().Length > 10)
                 return false;
 
             if (!DateTime.TryParse($"{parts[1]} {parts[2]}", out var fechaHora))
                 return false;
 
-            DateTime fechaHoraReducida = fechaHora.Date + new TimeSpan(fechaHora.Hour, fechaHora.Minute, 0);
+            // DateTime fechaHoraReducida = fechaHora.Date + new TimeSpan(fechaHora.Hour, fechaHora.Minute, 0);
 
             if (!int.TryParse(parts[3], out var tipo))
                 return false;
 
+            if (!int.TryParse(parts[4], out _)) return false;
+            if (!int.TryParse(parts[6], out _)) return false;
+
             record = new RegistroBiometrico(
-                parts[0],
-                fechaHoraReducida,
+                parts[0].Trim(),
+                fechaHora,
                 tipo,
                 parts[4],
                 parts[5],
