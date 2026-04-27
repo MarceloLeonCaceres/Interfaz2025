88fa8cd80e16497c38cb5e78c7d21634cc769fe5
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2026-04-27 09:43:28 -0500

    add AdminUser y Globales en Domain

---
+++ Domain/Administradores/AdminUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Administradores
+{
+    public class AdminUser
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public int OTAdmin { get; set; }
+        public string Privilegios { get; set; }
+    }
+}
---
+++ Domain/Administradores/Globales.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Administradores
+{
+    public static class Globales
+    {
+        public static string CodAdminLog { get; set; }
+        public static string AdminNom { get; set; }
+        public static int TAdminLogeado { get; set; }
+        public static string AdminPass { get; set; }
+        public static string PrivAdmin { get; set; }
+    }
+}
