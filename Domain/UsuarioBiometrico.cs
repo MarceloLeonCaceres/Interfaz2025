a05c0f835f0420fb7d958f6d17f7a96c6ffb0f1d
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2026-04-28 10:01:36 -0500

    add UsuarioBiometrico en Domain

---
+++ Domain/UsuarioBiometrico.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class UsuarioBiometrico
+    {
+        public string Pin { get; set; }
+        public string Name { get; set; }
+        public string MVerifyPass { get; set; }
+        public string CardNumber { get; set; }
+        public int Privilege { get; set; }
+        public int EnPrivilege { get; set; }
+    }
+}
